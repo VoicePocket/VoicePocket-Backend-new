fe57075759b95a62607e474bab79fe22e57d99a9
KyungMin Lee <hello.tidavid@gmail.com>
2024-03-29 21:27:41 +0900

    REFACT: Swagger 수정

--- voicepocket/src/main/java/com/vp/voicepocket/global/config/SwaggerConfiguration.java
+++ voicepocket/src/main/java/com/vp/voicepocket/global/config/SwaggerConfiguration.java
@@ -1,17 +1,36 @@
 package com.vp.voicepocket.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfiguration {
+
     @Bean
     public OpenAPI demoOpenAPI() {
+
+        var info = new Info().title("Voice Pocket project API")
+            .description("Voice Pocket application").version("v0.0.1");
+
+        var jwtScheme = "jwtAuth";
+        var securityRequirement = new SecurityRequirement().addList(jwtScheme);
+        var components = new Components()
+            .addSecuritySchemes(jwtScheme, new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .scheme("Bearer")
+                .bearerFormat("JWT"));
+
         return new OpenAPI()
-                .info(new Info().title("Voice Pocket project API")
-                        .description("Voice Pocket application")
-                        .version("v0.0.1"));
+            .components(new Components())
+            .info(info)
+            .addSecurityItem(securityRequirement)
+            .components(components);
     }
 }
